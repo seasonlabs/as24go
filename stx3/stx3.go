fe6433862f0555d67f20e7769bb6ae109357fe30
Victor Castell <victorcoder@gmail.com>
2012-10-07 01:09:44 +0200

    Moved xmlname properties to field tags to prevent mgo from storing it

--- stx3/stx3.go
+++ stx3/stx3.go
@@ -7,85 +7,70 @@ import (
 type Stx3 struct {
 	XMLName     xml.Name `xml:"stx3"`
 	VehicleData struct {
-		XMLName  xml.Name `xml:"vehicle_data"`
 		Vehicles struct {
-			XMLName xml.Name  `xml:"vehicles"`
 			Vehicle []Vehicle `xml:"vehicle"`
-		}
-	}
+		} `xml:"vehicles"`
+	} `xml:"vehicle_data"`
 }
 
 type Vehicle struct {
-	XMLName         xml.Name `xml:"vehicle"`
-	DealerId        string   `xml:"dealer_id"`
-	OwnersvehicleId string   `xml:"ownersvehicle_id"`
-	Type            string   `xml:"type"`
-	Category        string   `xml:"category"`
-	Body            string   `xml:"body"`
-	Brand           string   `xml:"brand"`
-	Model           string   `xml:"model"`
-	BodyColor       string   `xml:"body_color"`
-	BodyColorgroup  string   `xml:"body_colorgroup"`
-	Doors           string   `xml:"doors"`
-	GearType        string   `xml:"gear_type"`
-	Gears           string   `xml:"gears"`
-	FuelType        string   `xml:"fuel_type"`
-	Kilowatt        string   `xml:"kilowatt"`
-	Cylinder        string   `xml:"cylinder"`
-	Capacity        string   `xml:"capacity"`
+	DealerId        string `xml:"dealer_id" bson:"dealer_id"`
+	OwnersvehicleId string `xml:"ownersvehicle_id" bson:"ownersvehicle_id"`
+	Type            string `xml:"type"`
+	Category        string `xml:"category"`
+	Body            string `xml:"body"`
+	Brand           string `xml:"brand"`
+	Model           string `xml:"model"`
+	BodyColor       string `xml:"body_color" bson:"body_color"`
+	BodyColorgroup  string `xml:"body_colorgroup" bson:"body_colorgroup"`
+	Doors           string `xml:"doors"`
+	GearType        string `xml:"gear_type" bson:"gear_type"`
+	Gears           string `xml:"gears"`
+	FuelType        string `xml:"fuel_type" bson:"fuel_type"`
+	Kilowatt        string `xml:"kilowatt"`
+	Cylinder        string `xml:"cylinder"`
+	Capacity        string `xml:"capacity"`
 	Consumption     struct {
-		XMLName xml.Name `xml:"consumption"`
-		Liquid  struct {
-			XMLName    xml.Name `xml:"liquid"`
-			Urban      string   `xml:"urban"`
-			ExtraUrban string   `xml:"extra_urban"`
-			Combined   string   `xml:"combined"`
-		}
-		Electric struct {
-			XMLName    xml.Name `xml:"electric"`
-			Urban      string   `xml:"urban"`
-			ExtraUrban string   `xml:"extra_urban"`
-			Combined   string   `xml:"combined"`
-		}
-	}
+		Liquid   Consumption `xml:"liquid"`
+		Electric Consumption `xml:"electric"`
+	} `xml:"consumption"`
 	Mileage             string `xml:"mileage"`
-	InitialRegistration string `xml:"initial_registration"`
+	InitialRegistration string `xml:"initial_registration" bson:"initial_registration"`
 	Notes               string `xml:"notes"`
-	LicenseNumber       string `xml:"license_number"`
+	LicenseNumber       string `xml:"license_number" bson"license_number"`
 	Prices              struct {
-		XMLName xml.Name `xml:"prices"`
-		Price   []Price  `xml:"price"`
-	}
+		Price []Price `xml:"price"`
+	} `xml:"prices"`
 	Media struct {
-		XMLName xml.Name `xml:"media"`
-		Images  struct {
-			XMLName xml.Name `xml:"images"`
-			Image   []Image  `xml:"image"`
-		}
-	}
-	KerbWeight string `xml:"kerb_weight"`
+		Images struct {
+			Image []Image `xml:"image"`
+		} `xml:"images"`
+	} `xml:"media"`
+	KerbWeight string `xml:"kerb_weight" bson:"kerb_weight"`
 	Seats      string `xml:"seats"`
 	Equipments struct {
-		XMLName   xml.Name    `xml:"equipments"`
 		Equipment []Equipment `xml:"equipment"`
-	}
+	} `xml:"equipments"`
 }
 
 type Price struct {
-	XMLName  xml.Name `xml:"price"`
-	Type     string   `xml:"type"`
-	Currency string   `xml:"currency"`
-	Value    string   `xml:"value"`
+	Type     string `xml:"type"`
+	Currency string `xml:"currency"`
+	Value    string `xml:"value"`
 }
 
 type Equipment struct {
-	XMLName xml.Name `xml:"equipment"`
-	Text    string   `xml:"text"`
+	Text string `xml:"text"`
 }
 
 type Image struct {
-	XMLName xml.Name `xml:"image"`
-	Local   string   `xml:"local"`
+	Local string `xml:"local"`
+}
+
+type Consumption struct {
+	Urban      string `xml:"urban"`
+	ExtraUrban string `xml:"extra_urban" bson:"extra_urban"`
+	Combined   string `xml:"combined"`
 }
 
 func (stx3 *Stx3) Marshal() ([]byte, error) {
